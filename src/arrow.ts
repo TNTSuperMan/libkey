9be52c5cfeaa663562586e6253f94cc5565ccf12
TNTSuperMan <tntsuperga@gmail.com>
2025-01-10 15:54:59 +0900

    toLowerに変える(ミス)

--- src/arrow.ts
+++ src/arrow.ts
@@ -3,5 +3,5 @@ import type { State } from "./state";
 export const updateArrows = (e: KeyboardEvent, state: State, be: boolean) => {
     const res = /^Arrow(\w+)/.exec(e.key);
     if(res)
-        Reflect.set(state.arrow, res[1], be);
+        Reflect.set(state.arrow, res[1].toLowerCase(), be);
 }
